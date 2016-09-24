90e0371ef7e302d640d316401b957e104c664106
oingbong <oingbong@gmail.com>
2016-09-24 16:59:09 +0900

    LoginFilter.java 에서 if 조건문 수정

--- diningTable/src/main/java/com/dt/filter/LoginFilter.java
+++ diningTable/src/main/java/com/dt/filter/LoginFilter.java
@@ -24,12 +24,14 @@ public class LoginFilter implements Filter {
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		Object mNo = (Object) (((HttpServletRequest)request).getSession()).getAttribute("mNo");
-		if(mNo==null){
+		Object tNo = (Object) (((HttpServletRequest)request).getSession()).getAttribute("tNo");
+		if(mNo==null && tNo==null){ // 개인회원 혹은 레스토랑회원이 아닐경우
 			(((HttpServletRequest)request).getSession()).setAttribute("msg", "로그인 하고 사용하시기 바랍니다.");
 			((HttpServletResponse)response).sendRedirect("/diningTable/login.do");
 		}else{
 			chain.doFilter(request, response);
 		}
+		
 	}
 
 	@Override
